03915726e9f8605f892712c084001b29a97b8128
Laura <lauraalspaugh2002@icloud.com>
2024-02-28 13:39:58 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <header>
+    <Navbar />
+  </header>
+  <main>
+    <router-view />
+  </main>
+  <footer class="text-light">
+    <p class="m-5 fs-5 footer-style"><i class="mdi mdi-copyright"></i> This email was created by Laura Alspaugh</p>
+  </footer>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { AppState } from './AppState'
+import Navbar from './components/Navbar.vue'
+
+export default {
+  setup() {
+    return {
+      appState: computed(() => AppState)
+    }
+  },
+  components: { Navbar }
+}
+</script>
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+:root {
+  --main-height: calc(100vh - 32px - 64px);
+}
+
+
+footer {
+  display: grid;
+  place-content: center;
+  height: 32px;
+  background-color: #E6B0AA;
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container-fluid">
+    <section class="row">
+      <div class="col-12 p-4">
+        <p class="fs-1 newsletter text-center mb-0">January</p>
+        <p class="fs-1 newsletter text-center p-0 mt-0">Newsletter.</p>
+      </div>
+    </section>
+    <section class="row">
+      <div class="col-12 text-center">
+        <img class="img-fluid"
+          src="https://images.unsplash.com/photo-1571769710016-43cbfbfcf02d?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YWVzdGhldGljJTIwYmlrZXxlbnwwfHwwfHx8MA%3D%3D"
+          alt="">
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.newsletter {
+  color: #BDC3C7;
+}
+
+img {
+  height: 500px;
+  width: 60%;
+  position: center;
+  object-fit: cover;
+}
+</style>
